de5086bbe568dd086b121f73c12ff2daf20678aa
DaImata <matheusytplanzo@gmail.com>
2020-12-10 06:05:14 -0300

    Progresso

--- app/src/main/java/com/example/tcc/activity/TelaInicial.java
+++ app/src/main/java/com/example/tcc/activity/TelaInicial.java
@@ -2,20 +2,25 @@ package com.example.tcc.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.tcc.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
-
-import static java.security.AccessController.getContext;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class TelaInicial extends AppCompatActivity {
+    private MaterialSearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +33,43 @@ public class TelaInicial extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
         fragmentTransaction.replace(R.id.viewPager, new com.example.tcc.fragment.TelaInicial()).commit();
+
+        //Configurar Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        inicializarComponentes();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        switch (item.getItemId()) {
+            case R.id.menuCarrinho:
+                fragmentTransaction.replace(R.id.viewPager, new com.example.tcc.fragment.Carrinho()).commit();
+                return true;
+        }
+        return false;
+        }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+
+        //Configurar Pesquisa
+        MenuItem item = menu.findItem(R.id.menuPesquisar);
+        searchView.setMenuItem(item);
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     //Criação do bottom navigation view
-    private void configuraBottomNavigation(){
+    private void configuraBottomNavigation() {
         BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavigation);
 
         //Configs iniciais bottom navigation
@@ -42,7 +80,7 @@ public class TelaInicial extends AppCompatActivity {
     }
 
     //Tratamento navegação bottom navigation
-    private void habilitarNavegacao(BottomNavigationViewEx viewEx){
+    private void habilitarNavegacao(BottomNavigationViewEx viewEx) {
         viewEx.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -50,7 +88,7 @@ public class TelaInicial extends AppCompatActivity {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.ic_home:
                         fragmentTransaction.replace(R.id.viewPager, new com.example.tcc.fragment.TelaInicial()).commit();
                         return true;
@@ -67,5 +105,9 @@ public class TelaInicial extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void inicializarComponentes(){
+        searchView = findViewById(R.id.materialSearchView);
     }
 }
